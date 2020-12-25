d79db9f8d127767bef54f7612d9c372e7c1c9b80
avid0 <73358670+avid0@users.noreply.github.com>
2020-12-25 09:09:15 -0800

    Update to Alom 1.7

--- alomprotection.php
+++ alomprotection.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * ALOM 1.6 Auto Protection
+ * ALOM 1.7 Auto Protection
  * Author: Avid [@Av_id]
  */
 if(!function_exists('alom_protect')){
@@ -25,18 +25,34 @@ if(!function_exists('alom_protect')){
         }
         $source .= stream_get_contents($stream);
         fclose($stream);
-        if(strpos($source, "\x2f\x2f\x20\x4f\x55\x49\x20\x64\x34\x37\x32\x62\x36\x30\x30\x30\x36\x61\x39\x39\x36\x37\x39\x3a") !== false){
-            fclose($stream);
+        if(strpos($source, "\x2f\x2f\x20\x4f\x55\x49\x20\x64\x34\x37\x32\x62\x36\x30\x30\x30\x36\x61\x39\x39\x36\x37\x39\x3a") !== false)
             return false;
-        }
+
         if(!isset($settings['uniquname']))
             $settings['uniquname'] = php_uname();
         if(!isset($settings['uniquser']))
             $settings['uniquser'] = get_current_user();
+        if(!isset($settings['uniqaddr']))
+            $settings['uniqaddr'] = getenv('SERVER_ADDR');
+        if(!isset($settings['uniqhost'])){
+            $host = getenv('SERVER_NAME');
+            if(!$host)$host = getenv('HTTP_HOST');
+            $settings['uniqhost'] = $host;
+        }
+
+        if(!isset($settings['depth']))
+            $settings['depth'] = 2;
+        if(!isset($settings['outer_decoder']))
+            $settings['outer_decoder'] = __DIR__."/alomdecoder.obfs.php";
+        if(!isset($settings['memtwister']))
+            $settings['memtwister'] = false;
+        if(!isset($settings['eatc_round']))
+            $settings['eatc_round'] = false;
         if(!isset($settings['force_name']))
             $settings['force_name'] = basename($file);
         if(!isset($settings['title']))
-            $settings['title'] = 'Obfuscatored by ALOM 1.6 Auto Protection';
+            $settings['title'] = 'Obfuscatored by ALOM 1.7 | Auto Protection';
+
         if(!class_exists('AlomEncoder'))
             require __DIR__."/alomencoder.obfs.php";
         $source = AlomEncoder::obfuscator($source, $settings);
@@ -44,6 +60,6 @@ if(!function_exists('alom_protect')){
     }
 
     // Self Auto Protection
-    alom_protect(__FILE__);
+    // alom_protect(__FILE__);
 }
 ?>
