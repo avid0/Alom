6813c7255d73a13d20443a264c28a90eb5f3e362
avid0 <73358670+avid0@users.noreply.github.com>
2021-07-07 14:13:23 -0800

    Update expiredHelloWorld.php

--- example/expiredHelloWorld.php
+++ example/expiredHelloWorld.php
@@ -2,19 +2,21 @@
 require_once "../alomencoder.obfs.php";
 
 $settings = [
-  'expiration' => time() + 60, // 1 min
-  'title' => 'Expired Hello World',
-  'copyright' => 'https://github.com/avid0/Alom examples',
-  'author' => 'Avid'
+  'license' => [
+    'title' => 'Expired Hello World',
+    'author' => 'Avid',
+    'copyright' => 'https://github.com/avid0/Alom examples'
+  ],
+  'date_domain' => [
+    'expiration' => time() + 60, // 1 min
+  ]
 ];
 
-$code = '<?php
-$var = "Hello I am hidden! find me";
-print "Hello world!";
-unset($var);
-?>';
-
-$obfuscatored = AlomEncoder::Obfuscator($code, $settings);
+$obfuscatored = AlomEncoder::Obfuscator(function(){
+  $var = "Hello I am hidden! find me";
+  print "Hello world!";
+  unset($var);
+}, $settings);
 file_put_contents('expiredHelloWorld.obfs.php', $obfuscatored);
 
 ?>
