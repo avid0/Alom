bf03b665865b6ee5137ba2fbd3f27590ff09a887
avid0 <73358670+avid0@users.noreply.github.com>
2021-11-14 23:07:53 +0330

    Updated to version 2.5.3 fixed an bug

--- alomtools.php
+++ alomtools.php
@@ -3,8 +3,8 @@
  * ALOM 2.5 Tools
  * Author: Avid [@Av_id]
  */
-if(!defined('ALOM_VERSION')){define('ALOM_VERSION', '2.5.2');}
-if(!defined('ALOM_VERSION_NUMBER')){define('ALOM_VERSION_NUMBER', 20502);}
+if(!defined('ALOM_VERSION')){define('ALOM_VERSION', '2.5.3');}
+if(!defined('ALOM_VERSION_NUMBER')){define('ALOM_VERSION_NUMBER', 20503);}
 if(!class_exists('AlomEncoder'))
     require __DIR__."/alomencoder.obfs.php";
 
