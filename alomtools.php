48b04a08c5a88b743d910ac198f1021f0e98f874
avid0 <73358670+avid0@users.noreply.github.com>
2021-12-30 18:46:33 +0330

    Updated to version 2.6.4

--- alomtools.php
+++ alomtools.php
@@ -3,8 +3,8 @@
  * ALOM Tools
  * Author: Avid [@Av_id]
  */
-if(!defined('ALOM_VERSION')){define('ALOM_VERSION', '2.6.3');}
-if(!defined('ALOM_VERSION_NUMBER')){define('ALOM_VERSION_NUMBER', 20603);}
+if(!defined('ALOM_VERSION')){define('ALOM_VERSION', '2.6.4');}
+if(!defined('ALOM_VERSION_NUMBER')){define('ALOM_VERSION_NUMBER', 20604);}
 if(!class_exists('AlomEncoder'))
     require __DIR__."/alomencoder.obfs.php";
 
